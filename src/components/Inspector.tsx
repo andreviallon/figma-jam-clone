7ea67fe2d2d54d38bd9263b1f132f50ddd4ac79a
Andre Viallon <viallon.andre@gmail.com>
2022-02-08 20:21:15 +0100

    chore: basic inspector

---
+++ src/components/Inspector.tsx
@@ -0,0 +1,9 @@
+export const Inspector = () => {
+  return (
+    <div className="flex z-50 bg-gray-100 w-full h-full">
+      <div className="flex flex-col w-full">
+        <p>inspector</p>
+      </div>
+    </div>
+  );
+};
