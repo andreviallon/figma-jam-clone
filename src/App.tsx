b1c7e082af55d08b2b3441f794c23d92a6b89483
Andre Viallon <viallon.andre@gmail.com>
2022-02-11 18:51:44 +0100

    chore: add toolbar and inspector too app comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.scss";
 import { Header } from "./components/Header";
 import { Scene } from "./components/Scene";
+import { ToolBar } from "./components/ToolBar";
+import { Inspector } from "./components/Inspector";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faCircle,
@@ -11,13 +13,19 @@ import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faCircle, faSquare, faArrowPointer, faHandPaper);
 
-function App() {
+const App = () => {
   return (
     <div className="w-full h-full">
       <Header />
-      <Scene />
+      <div className="flex">
+        <ToolBar />
+        <Scene />
+        <div className="w-80">
+          <Inspector />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
