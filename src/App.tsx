3ebdd8c6041cbdca311140265a253fdf1e25af64
Andre Viallon <viallon.andre@gmail.com>
2022-02-11 18:51:30 +0100

    chore: register fontawesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import "./App.scss";
 import { Header } from "./components/Header";
 import { Scene } from "./components/Scene";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCircle,
+  faHandPaper,
+  faSquare,
+} from "@fortawesome/free-regular-svg-icons";
+import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCircle, faSquare, faArrowPointer, faHandPaper);
 
 function App() {
   return (
