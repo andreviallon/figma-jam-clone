ed6cb9337f75818ecd838c810a8c3ed62980cee9
Andre Viallon <viallon.andre@gmail.com>
2022-02-14 20:00:07 +0100

    chore: register tool state

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,19 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import { stageReducer } from "./StageReducer";
-import { StageState } from "./StageStateModel";
+import { stageReducer } from "./stage/StageReducer";
+import { StageState } from "./stage/StageStateModel";
+import { toolReducer } from "./tool/ToolReducer";
+import { ToolState } from "./tool/ToolStateModel";
 
 export interface IState {
   stage: StageState;
+  tool: ToolState;
 }
 
 export const rootReducer = combineReducers({
   stage: stageReducer,
+  tool: toolReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
