48859a8fd67899ab50e7b1cadf17bf4e62923e67
Andre Viallon <viallon.andre@gmail.com>
2022-02-14 19:59:59 +0100

    chore: connect ToolBar to Tool State

--- src/components/ToolBtn.tsx
+++ src/components/ToolBtn.tsx
@@ -1,12 +1,21 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ToolEnum } from "../models/tool";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface Props {
   faIcon: IconProp;
-  isSelected: boolean;
+  tool: ToolEnum;
+  selectedTool: ToolEnum;
 }
 
-export const ToolBtn: React.FC<Props> = ({ faIcon, isSelected }) => {
+export const ToolBtn: React.FC<Props> = ({ faIcon, tool, selectedTool }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  useEffect(() => {
+    setIsSelected(tool === selectedTool);
+  }, [tool, selectedTool]);
+
   const classes = `cursor-pointer text-lg w-14 h-14 ${
     isSelected
       ? "bg-indigo-600 text-white"
