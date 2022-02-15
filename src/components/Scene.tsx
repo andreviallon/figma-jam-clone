a3b7bf418e86a6fe141f2720e8b998280ea8fd15
Andre Viallon <viallon.andre@gmail.com>
2022-02-15 16:45:10 +0100

    chore: remove unsed imports

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Layer, Stage } from "react-konva";
 import { Rectangle } from "./Rectangle";
 import { useDispatch, useSelector } from "react-redux";
