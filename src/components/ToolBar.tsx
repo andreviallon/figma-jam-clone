59100a20da6575f633702fafc3b3fa92f6f42f05
Andre Viallon <viallon.andre@gmail.com>
2022-02-15 16:02:41 +0100

    chore: connect toolbar to state

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,19 +1,42 @@
+import { ShapeEnum } from "../models/shape";
+import { ToolBtn } from "./ToolBtn";
+import { ToolShape } from "./ToolShape";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import { ToolEnum } from "../models/tool";
+import { selectTool } from "../state/tool/ToolActions";
+import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
 import {
   faCircle,
   faHandPaper,
   faSquare,
 } from "@fortawesome/free-regular-svg-icons";
-import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
-import { ShapeEnum } from "../models/shape";
-import { ToolBtn } from "./ToolBtn";
-import { ToolShape } from "./ToolShape";
 
 export const ToolBar = () => {
+  const dispatch = useDispatch();
+  const { selectedTool } = useSelector((state: RootState) => state.tool);
+
+  const setSelectedTool = (tool: ToolEnum) => {
+    dispatch(selectTool(tool));
+  };
+
   return (
     <div className="flex z-50 bg-gray-100 w-14">
       <div className="flex flex-col w-full">
-        <ToolBtn faIcon={faArrowPointer} isSelected={false} />
-        <ToolBtn faIcon={faHandPaper} isSelected={true} />
+        <div onClick={() => setSelectedTool(ToolEnum.POINTER)}>
+          <ToolBtn
+            faIcon={faArrowPointer}
+            tool={ToolEnum.POINTER}
+            selectedTool={selectedTool}
+          />
+        </div>
+        <div onClick={() => setSelectedTool(ToolEnum.MOVE)}>
+          <ToolBtn
+            faIcon={faHandPaper}
+            tool={ToolEnum.MOVE}
+            selectedTool={selectedTool}
+          />
+        </div>
         <div className="h-px border-b border-solid border-gray-300 mx-2 my-3" />
         <ToolShape faIcon={faSquare} shape={ShapeEnum.RECTANGLE} />
         <ToolShape faIcon={faCircle} shape={ShapeEnum.CIRCLE} />
