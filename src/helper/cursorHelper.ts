04a7f9ade32babcb6526de938da2e47417b2fa44
Andre Viallon <viallon.andre@gmail.com>
2022-02-16 17:41:29 +0100

    feat: cursor helper

---
+++ src/helper/cursorHelper.ts
@@ -0,0 +1,22 @@
+import { CursorEnum } from "../models/cursor";
+import { ToolEnum } from "../models/tool";
+
+export const setCursor = (selectedTool: ToolEnum) => {
+  console.log(selectedTool);
+
+  if (selectedTool === ToolEnum.POINTER) {
+    document.body.style.cursor = CursorEnum.DEFAULT;
+  }
+
+  if (selectedTool === ToolEnum.MOVE) {
+    document.body.style.cursor = CursorEnum.MOVE;
+  }
+
+  if (selectedTool === ToolEnum.RECTANGLE || selectedTool === ToolEnum.CIRCLE) {
+    document.body.style.cursor = CursorEnum.CROSSHAIR;
+  }
+};
+
+export const resetCursor = () => {
+  document.body.style.cursor = CursorEnum.DEFAULT;
+};
---
+++ src/models/cursor.ts
@@ -0,0 +1,5 @@
+export enum CursorEnum {
+  DEFAULT = "default",
+  MOVE = "move",
+  CROSSHAIR = "crosshair",
+}
