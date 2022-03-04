48546d61eb7094ffd7b3268e3877c801d6b2fd8b
Andre Viallon <viallon.andre@gmail.com>
2022-03-04 16:40:19 +0100

    chore: update app title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <div className="z-50 bg-zinc-800 p-4">
-      <h1 className="font-bold text-lg text-white">FigJam Clone</h1>
+      <h1 className="font-bold text-lg text-white">Figma Clone</h1>
     </div>
   );
 };
