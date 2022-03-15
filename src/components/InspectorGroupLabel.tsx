3e85e8ffb783155dc89b4fe09ee6b167ebb97e30
Andre Viallon <viallon.andre@gmail.com>
2022-03-15 16:59:05 +0100

    style: remove inspector group margin bottom

--- src/components/InspectorGroupLabel.tsx
+++ src/components/InspectorGroupLabel.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export const InspectorGroupLabel: React.FC<Props> = ({ label }) => {
-  return <p className="text-xs font-medium w-full mb-1">{label}</p>;
+  return <p className="text-xs font-medium w-full">{label}</p>;
 };
