eb47fdd89a21cc6316f54ba729c749a2d905cddc
Andre Viallon <viallon.andre@gmail.com>
2022-04-03 17:15:57 +0200

    chore: handle disable toolbar btn

--- src/components/ToolBtn.tsx
+++ src/components/ToolBtn.tsx
@@ -2,27 +2,42 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ToolEnum } from "../models/tool";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { ActionEnum } from "../models/action";
 
 interface Props {
   faIcon: IconProp;
-  tool: ToolEnum;
-  selectedTool: ToolEnum;
+  action?: ActionEnum;
+  tool?: ToolEnum;
+  selectedTool?: ToolEnum;
+  disabled?: boolean;
 }
 
-export const ToolBtn: React.FC<Props> = ({ faIcon, tool, selectedTool }) => {
+export const ToolBtn: React.FC<Props> = ({
+  faIcon,
+  tool,
+  selectedTool,
+  disabled,
+}) => {
   const [isSelected, setIsSelected] = useState(false);
 
   useEffect(() => {
-    setIsSelected(tool === selectedTool);
+    setIsSelected(selectedTool ? tool === selectedTool : false);
   }, [tool, selectedTool]);
 
-  const classes = `cursor-pointer text-lg w-14 h-14 ${
-    isSelected
+  const getClasses = (): string => {
+    let classes = "cursor-pointer text-lg w-14 h-14 ";
+
+    classes += isSelected
       ? "bg-indigo-600 text-white"
-      : "text-neutral-900 hover:bg-neutral-200"
-  }`;
+      : "text-neutral-900 hover:bg-neutral-200";
+
+    classes += disabled ? " opacity-30 cursor-not-allowed" : "";
+
+    return classes;
+  };
+
   return (
-    <button className={classes}>
+    <button className={getClasses()} disabled={disabled}>
       <FontAwesomeIcon icon={faIcon} />
     </button>
   );
